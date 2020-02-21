73cd025b4307fb20923dee85f63de2419f7d151c
ShatterPlayer <shatterplayer@gmail.com>
2020-02-21 20:56:19 +0000

    Create Blog “testing-netlifycms”

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,14 @@ module.exports = {
       },
     },
     `gatsby-plugin-netlify-cms`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/blog`,
+        name: `blog_entries`,
+      },
+    },
+    `gatsby-transformer-remark`,
   ],
 }
